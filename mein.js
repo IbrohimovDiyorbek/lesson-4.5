57a8b478fbf25731c356a71370fe672160699e8b
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-03-20 22:57:48 +0500

    it is a hamowork

---
+++ mein.js
@@ -0,0 +1,10 @@
+let name1 = prompt("Ismingizni kiriyying!")
+let letter = prompt("Istagan harfni kiriting!")
+
+let check = name1.toLowerCase().includes(letter.toLowerCase());
+
+if (check) {
+    alert("Siz kirtgan harf mavjud😃")
+} else (
+    alert("Siz kiritgan harf mavjud emas😞")
+)
